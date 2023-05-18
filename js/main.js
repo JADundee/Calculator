7379840e0c8c4d2cdf109ee6b49dddf221613e67
JADundee <Jadundee@icloud.com>
2023-05-18 08:34:45 -0600

    Added number button input and clear functions

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const initApp = () => {
+
+    const currentValueElement = document.querySelector(`.currentValue`);
+    const previousValueElement = document.querySelector(`.previousValue`);
+    let itemArray = [];
+    const equationArray = [];
+    let newNumberFlag = false;
+
+    const inputButtons = document.querySelectorAll(`.number`);
+    inputButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            
+            const newInput = event.target.textContent;
+            if (newNumberFlag) {
+                currentValueElement.value = newInput;
+                newNumberFlag = false;
+            } else {
+                currentValueElement.value = 
+                    currentValueElement.value == 0
+                    ? newInput
+                    : `${currentValueElement.value}${newInput}`;
+            }
+        });
+    });
+
+    const clearButtons = document.querySelectorAll(`.clear, .clearEntry`);
+    clearButtons.forEach(button => {
+        button.addEventListener(`click`, (event) => {
+            currentValueElement.value = 0;
+            if (event.target.classList.contains(`clear`)) {
+                previousValueElement.textContent = ``;
+                itemArray = [];
+            }
+        });
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
